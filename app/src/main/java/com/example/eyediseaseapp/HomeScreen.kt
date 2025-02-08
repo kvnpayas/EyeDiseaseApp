49feeeddba40428b75b08fd04676d300fc489859
kvnpayas <kvnpayas@gmail.com>
2025-02-09 01:36:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/eyediseaseapp/HomeScreen.kt
@@ -0,0 +1,221 @@
+package com.example.eyediseaseapp
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.compose.ui.res.colorResource
+import androidx.navigation.compose.rememberNavController
+import com.example.eyediseaseapp.ui.theme.EyeDiseaseAppTheme
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.home_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp)
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White.copy(alpha = 0.8f)
+                ),
+                elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
+                border = BorderStroke(1.dp, colorResource(id = R.color.darkPrimary))
+
+            ) {
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Eye Disease Detection",
+                        fontSize = 35.sp,
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(fontWeight = FontWeight.ExtraBold),
+                    )
+                    Text(
+                        text = "App",
+                        fontSize = 35.sp,
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(fontWeight = FontWeight.ExtraBold),
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.cataract_img2),
+                    contentDescription = "image 1",
+                    modifier = Modifier
+                        .width(130.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.glaucoma2_img),
+                    contentDescription = "image 1",
+                    modifier = Modifier
+                        .width(130.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.LightGray,
+                ),
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                shape = RoundedCornerShape(10.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "CATARACT AND GLAUCOMA",
+//                        color = colorResource(id = R.color.darkPrimary),
+                        color = Color.Black,
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(fontWeight = FontWeight.ExtraBold),
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_camera_alt_24),
+                        contentDescription = "image 1",
+                        colorFilter = ColorFilter.tint(Color.DarkGray),
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .width(80.dp)
+                            .height(80.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Button(
+                        modifier = Modifier.padding(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(id = R.color.darkPrimary)
+                        ),
+
+                        onClick = { navController.navigate("camera") }
+                    ) {
+                        Text(
+                            text = "CAMERA",
+                            color = Color.White,
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(fontWeight = FontWeight.ExtraBold),
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_collections_24),
+                        contentDescription = "image 1",
+                        colorFilter = ColorFilter.tint(Color.DarkGray),
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .width(80.dp)
+                            .height(80.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Button(
+                        modifier = Modifier.padding(5.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = colorResource(id = R.color.darkPrimary)
+                        ),
+                        onClick = { navController.navigate("camera") }
+                    ) {
+                        Text(
+                            text = "UPLOAD IMAGE",
+                            color = Color.White,
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(fontWeight = FontWeight.ExtraBold),
+                        )
+                    }
+                }
+            }
+//            Button(onClick = { navController.navigate("camera") }) {
+//                Text(text = "Start Eye Scan")
+//            }
+//            Spacer(modifier = Modifier.height(16.dp))
+//            Button(onClick = { navController.navigate("aboutUs") }) {
+//                Text(text = "About Us")
+//            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    EyeDiseaseAppTheme {
+        HomeScreen(navController = rememberNavController())
+    }
+}
